111faa638a4287d49c8461a12c7ee7148633371a
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-08-30 09:23:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,7 +27,6 @@ git clone https://github.com/Lienol/openwrt-package.git package/lienol
 git clone https://github.com/Lienol/luci-theme-darkmatter.git package/luci-theme-darkmatter
 git clone https://github.com/mit-ywtm/luci-theme-rosy.git package/luci-theme-rosy
 git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly.git package/luci-theme-Butterfly
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark.git package/luci-theme-Butterfly-dark
